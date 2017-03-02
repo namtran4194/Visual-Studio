6073e75d42e02651373648ee90f8494ad946895e
Trần Xuân Nam <namtran4194@gmail.com>
2017-03-02 11:21:03 +0700

    update changes

--- DemoADO.NET/Site.Master.designer.cs
+++ DemoADO.NET/Site.Master.designer.cs
@@ -13,13 +13,13 @@ namespace DemoADO.NET {
     public partial class Site {
         
         /// <summary>
-        /// form1 control.
+        /// mainForm control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+        protected global::System.Web.UI.HtmlControls.HtmlForm mainForm;
         
         /// <summary>
         /// ContentPlaceHolder1 control.
